4f1c5b4d6cde57578976d84c992ddbcfe135685f
closertb <closertb@sina.com>
2018-01-05 22:41:35 +0800

    three.js 测试案列

--- awesomeCanvas/rotateBall/index.js
+++ awesomeCanvas/rotateBall/index.js
@@ -13,14 +13,6 @@ var DemoThree = {
     camera: {},
     scene: {},
     light: {},
-    init: function () {
-        this.createRender();
-        this.createCamera(true);
-        this.createScene();
-        this.createLight();
-        this.createObject();
-        animation();
-    },
     createRender:function () {
         var tar = document.getElementById('canvas-frame');
         this.width = tar.clientWidth;
@@ -29,6 +21,7 @@ var DemoThree = {
             antialias : true
         });
         this.renderer.setSize(this.width, this.height);
+        this.renderer.shadowMapEnabled = true;
         tar.appendChild(this.renderer.domElement);
 
         this.renderer.setClearColor(0xFFFFFF, 1.0);
@@ -45,7 +38,7 @@ var DemoThree = {
         //camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 10, 1000 );
         this.camera.position.x = 0;
         this.camera.position.y = 0;
-        this.camera.position.z = 600;
+        this.camera.position.z = 500;
         this.camera.up.x = 0;
         this.camera.up.y = 1;
         this.camera.up.z = 0;
@@ -59,23 +52,53 @@ var DemoThree = {
         this.scene = new THREE.Scene();
     },
     createLight: function () {
-        this.light = new THREE.DirectionalLight(0xFF0000, 1.0, 0);
-        this.light.position.set(100, 100, 200);
+        this.light  = new THREE.PointLight(0x0000ff,1, 400);
+        this.light.position.set(100, 200, 100);
+        this.light.castShadow = true;
         this.scene.add(this.light);
     },
-    createObject:function () {
-        var geometry = new THREE.CylinderGeometry( 100,150,400);
+    createMesh:function () {
+        this.mesh = new THREE.Object3D();
+        this.scene.add(this.mesh);
+    },
+/*    createObject:function () {
+        var geometry = new THREE.BoxGeometry( 100, 100, 100 );
         var material = new THREE.MeshLambertMaterial( { color:0xFFFF00} );
         var mesh = new THREE.Mesh( geometry,material);
-        mesh.position = new THREE.Vector3(0,0,0);
+        mesh.position.set(200, 0, 0);
+        mesh.castShadow = true;
         this.scene.add(mesh);
+
+        var geometrya = new THREE.BoxGeometry( 100, 100, 100 );
+        var materiala = new THREE.MeshLambertMaterial( { color:0x00FF00} );
+        var mesha = new THREE.Mesh( geometrya,materiala);
+        mesha.position.set(0, 0, 0);
+        this.scene.add(mesha);
+    },*/
+    createObject:function () {
+        var geometry = new THREE.SphereGeometry(100, 8, 6);
+        this.mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial(0xffffff) );
+        var edges = new THREE.EdgesHelper( this.mesh, 0x0000ff );
+
+        this.scene.add(this.mesh);
+        this.mesh.add(edges);
+       // this.scene.add(edges);
+    },
+    init: function () {
+        this.createRender();
+        this.createCamera(true);
+        this.createScene();
+     //   this.createLight();
+        this.createObject();
+        this.renderer.render(this.scene, this.camera);
+        animation();
     }
 }
 
 function animation()
 {
     //renderer.clear();
-    DemoThree.camera.position.x =DemoThree.camera.position.x +1;
+    DemoThree.mesh.rotation.z =DemoThree.mesh.rotation.z +0.001;
     DemoThree.renderer.render(DemoThree.scene, DemoThree.camera);
     requestAnimationFrame(animation);
     DemoThree.stats.update();
