f0df977719f85681fd4bff4cdf3ff4db7be8564c
closertb <closertb@sina.com>
2018-01-23 23:04:59 +0800

    雨滴组件优化

--- gulpfile.js
+++ gulpfile.js
@@ -71,7 +71,7 @@ var path ={
     }
 }
 
-var editPath = path.awesomeCanvas.bubbling.path;  //要使用服务的组件路径
+var editPath = path.awesomeCanvas.weatherDrop.path;  //要使用服务的组件路径
 /**
  * name:新建组件的文件夹名称
  * eg: 一级目录组件创建 gulp create --name dirName 依据指定的微件文件夹名称生成对应的微件
